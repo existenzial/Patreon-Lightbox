3239d4e245bffb5861acf883ab1c554a5cc972c3
existenzial <ripley36706@gmail.com>
2016-12-31 16:57:25 -0800

    renamed gallery action variables to reflect current HTML structure

--- public/src/actions/galleryActions.js
+++ public/src/actions/galleryActions.js
@@ -8,6 +8,7 @@ function closeModal() {
   document.getElementById('lightbox').style.display = "none";
 }
 
+// SLIDESHOW - SET CURRENT SLIDE w/FIRST SLIDE
 var slideIndex = 1;
 showLightboxSlides(slideIndex);
 
@@ -24,25 +25,25 @@ function setCurrentSlide(n) {
 function showLightboxSlides(n) {
   var i;
   var slides = document.getElementsByClassName("gallery-thumbnail");
-  var dots = document.getElementsByClassName("dot");
+  var thumbnails = document.getElementsByClassName("lightbox-thumbnail");
   var slideTitleText = document.getElementById("slide-title");
   // slideshow end - go back to the beginning
-  if (n > slides.length) { slideIndex = 1 }
+  if (n > slides.length) { slideIndex = 1; }
   // slideshow err - go to end
-  if (n < 1) { slideIndex = slides.length }
+  if (n < 1) { slideIndex = slides.length; }
   // iterate through slides & set display to none
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  // iterate through dots, replace "active" class with ""
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+  // iterate through thumbnails, replace "active" class with ""
+  for (i = 0; i < thumbnails.length; i++) {
+    thumbnails[i].className = thumbnails[i].className.replace(" active", "");
   }
 
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  thumbnails[slideIndex-1].className += " active";
   // change caption text to title of current slide
-  slideTitleText.innerHTML = dots[slideIndex - 1].alt;
+  slideTitleText.innerHTML = thumbnails[slideIndex - 1].alt;
 }
 
 function lazyLoadGalleryImage(img) {
