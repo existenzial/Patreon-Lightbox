b6d3a04f3b803fc05010a74f27e3a34e9bf3c4df
existenzial <ripley36706@gmail.com>
2017-01-02 13:26:57 -0800

    fixed and clarified variable misnomers

--- public/src/common/Search.js
+++ public/src/common/Search.js
@@ -1,11 +1,13 @@
-// Search - User Input
+// Search
+// Wrapper + Input + Submit DOM Elements
 var Search = function() {
-  var row = document.getElementById("search-container");
+  var searchContainer = document.getElementById("search-container");
 
   var searchWrapper = document.createElement("div");
       searchWrapper.className = "search";
 
   var searchForm = document.createElement("form");
+      searchForm.id = "search";
       searchForm.setAttribute("action", "POST");
 
   var searchInput = document.createElement("input");
@@ -15,16 +17,15 @@ var Search = function() {
       searchInput.setAttribute("placeholder", "Search...");
 
   var searchSubmit = document.createElement("input");
-      searchInput.id = "search-submit";
-      searchInput.type = "submit";
-      searchInput.name = "load-gallery";
-      searchInput.value = "Load Gallery";
-      searchInput.onclick = handleSearch;
+      searchSubmit.id = "search-submit";
+      searchSubmit.type = "submit";
+      searchSubmit.name = "load-gallery";
+      searchSubmit.value = "Load Gallery";
 
   searchForm.appendChild(searchInput);
   searchForm.appendChild(searchSubmit);
-  row.appendChild(searchWrapper);
+  searchContainer.appendChild(searchWrapper);
 };
 
-Search();
-console.log("Search Loaded");
+//Search();
+//console.log("Search Loaded");
