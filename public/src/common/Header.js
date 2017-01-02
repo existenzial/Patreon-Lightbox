a0232135b437a02e7abf605d427984027ba8c434
existenzial <ripley36706@gmail.com>
2017-01-02 13:29:58 -0800

    fixed indentation

--- public/src/common/Header.js
+++ public/src/common/Header.js
@@ -3,25 +3,22 @@
 var Header = function() {
   var headerOuterWrapper = document.getElementById("header");
   var headerInnerWrapper = document.createElement("div");
-  headerInnerWrapper.id = "header-inner";
-  headerInnerWrapper.className = "nav navbar";
+      headerInnerWrapper.id = "header-inner";
+      headerInnerWrapper.className = "nav navbar";
 
   var headerLogo = document.createElement("img");
-  headerLogo.id = "header-logo";
-  headerLogo.src = "../patreon_logo.png";
-  headerLogo.setAttribute("alt", "Patreon Logo");
+      headerLogo.id = "header-logo";
+      headerLogo.src = "../patreon_logo.png";
+      headerLogo.setAttribute("alt", "Patreon Logo");
 
   var headerTitle = document.createElement("h1");
-  headerTitle.id = "header-title";
-  headerTitle.innerHTML = "Patreon Lightbox";
+      headerTitle.id = "header-title";
+      headerTitle.innerHTML = "Patreon Lightbox";
 
   headerInnerWrapper.appendChild(headerLogo);
   headerInnerWrapper.appendChild(headerTitle);
   headerOuterWrapper.appendChild(headerInnerWrapper);
 };
 
-// Header.constructor = Header;
-// Header.prototype.changeLogo = function() {};
-// Header.prototype.changeTitle = function() {};
 Header();
 console.log("Header Loaded");
