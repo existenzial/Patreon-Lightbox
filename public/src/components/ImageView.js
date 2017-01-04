94bc7c2312ff5ac629e31659e95125ea55e2a31d
existenzial <ripley36706@gmail.com>
2017-01-03 19:20:36 -0800

    removed for code cleanup

--- public/src/components/ImageView.js
+++
@@ -1,8 +0,0 @@
-var ImageView = function(imageData) {
-  var image = new Image(imageData);
-
-  var imgElement = document.createElement("img");
-  imgElement.id = image.id;
-  imgElement.src = image.mediaUrl;
-  imgElement.setAttribute("alt", image.title);
-};
