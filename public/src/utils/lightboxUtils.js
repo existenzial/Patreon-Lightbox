97a727a22d56181c88c72bad848488c32cf2dcbf
existenzial <ripley36706@gmail.com>
2017-01-03 19:24:13 -0800

    removed debug console.logs

--- public/src/utils/lightboxUtils.js
+++ public/src/utils/lightboxUtils.js
@@ -5,7 +5,6 @@ function createLightboxSlide(i) {
   var slide = document.createElement("div");
       slide.id = (i + 1); //"slide" +
       slide.className = "slide";
-  console.log(i + " : Slide container div created!");
   return slide;
 }
 
@@ -14,7 +13,6 @@ function createLightboxSlideTitle(image) {
   var slideTitle = document.createElement("p");
       slideTitle.className = "slide-title";
       slideTitle.innerHTML = image.title;
-  console.log("Slide title created!");
   return slideTitle;
 }
 
@@ -25,6 +23,5 @@ function createLightboxSlideImage(image) {
       slideImage.className = "slide-image";
       slideImage.src = image.mediaUrl;
       slideImage.setAttribute("alt", image.title);
-  console.log("Slide image created!");
   return slideImage;
 }
