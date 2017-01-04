0999abcc6c84e77d4339904f4314a4c5b62d686e
existenzial <ripley36706@gmail.com>
2017-01-03 19:15:55 -0800

    completed Search component

--- public/src/common/Search.js
+++ public/src/common/Search.js
@@ -1,11 +1,7 @@
 // Search
-// Wrapper + Input + Submit DOM Elements
+// Wrapper + Input DOM Elements
 var Search = function() {
   var searchContainer = document.getElementById("search-container");
-
-  var searchWrapper = document.createElement("div");
-      searchWrapper.className = "search";
-
   var searchForm = document.createElement("form");
       searchForm.id = "search";
       searchForm.setAttribute("action", "POST");
@@ -13,19 +9,11 @@ var Search = function() {
   var searchInput = document.createElement("input");
       searchInput.id = "search-term";
       searchInput.type = "text";
-      searchInput.name = "search-term";
       searchInput.setAttribute("placeholder", "Search...");
 
-  var searchSubmit = document.createElement("input");
-      searchSubmit.id = "search-submit";
-      searchSubmit.type = "submit";
-      searchSubmit.name = "load-gallery";
-      searchSubmit.value = "Load Gallery";
-
   searchForm.appendChild(searchInput);
-  searchForm.appendChild(searchSubmit);
-  searchContainer.appendChild(searchWrapper);
+  searchContainer.appendChild(searchForm);
 };
 
-//Search();
-//console.log("Search Loaded");
+Search();
+console.log("Search Loaded");
